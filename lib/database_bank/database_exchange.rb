2e1c0bdd6e05cbdfe0f257edf5bf8d89115e4e6e
Christopher Bailey <chris@hoteltonight.com>
2012-03-31 18:24:44 -0700

    Deal with proper currency object types for get_rate

--- lib/database_bank/database_exchange.rb
+++ lib/database_bank/database_exchange.rb
@@ -4,8 +4,14 @@ class Money
     module DatabaseBank
 
       class DatabaseExchange < Money::Bank::VariableExchange
-        def get_rate(from, to)
-          rate = DatabaseBank.exchange_rate_model.where(from_currency: from).where(to_currency: to).order(:sourced_at).last
+
+        # Retrieve the rate for the given currencies
+        #
+        # from_currency - a Currency object representing the currency being converted from
+        # to_iso_code - the ISO code of the currency to convert to
+        #
+        def get_rate(from_currency, to_iso_code)
+          rate = DatabaseBank.exchange_rate_model.where(from_currency: from_currency.iso_code).where(to_currency: to_iso_code).order(:sourced_at).last
           raise UnknownRate unless rate
           rate.rate
         end
