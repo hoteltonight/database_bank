e742a0ce95fcc318e845274776b718ac6a3590cb
Christopher Bailey <chris@hoteltonight.com>
2012-04-10 17:46:38 -0700

    Handle to currency as string or Currency when getting rate.

--- lib/database_bank/database_exchange.rb
+++ lib/database_bank/database_exchange.rb
@@ -8,10 +8,10 @@ class Money
         # Retrieve the rate for the given currencies
         #
         # from_currency - a Currency object representing the currency being converted from
-        # to_iso_code - the ISO code of the currency to convert to
+        # to_currency - a Currency or String with the ISO code of the currency to convert to
         #
-        def get_rate(from_currency, to_iso_code)
-          rate = DatabaseBank.exchange_rate_model.where(from_currency: from_currency.iso_code).where(to_currency: to_iso_code).order(:sourced_at).last
+        def get_rate(from_currency, to_currency)
+          rate = DatabaseBank.exchange_rate_model.where(from_currency: from_currency.iso_code).where(to_currency: to_currency.to_s).order(:sourced_at).last
           raise UnknownRate unless rate
           rate.rate
         end
